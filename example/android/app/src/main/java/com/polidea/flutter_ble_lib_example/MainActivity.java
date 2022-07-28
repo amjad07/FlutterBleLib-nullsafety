bb8d072244a7976a7476d10abf3bee56d99247e6
Muhammad Amjad <amjadakram07@gmail.com>
2022-07-28 22:05:16 +0500

    Initial commit

---
+++ example/android/app/src/main/java/com/polidea/flutter_ble_lib_example/MainActivity.java
@@ -0,0 +1,18 @@
+package com.polidea.flutter_ble_lib_example;
+
+//import android.os.Bundle;
+//import io.flutter.app.FlutterActivity;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+//
+//public class MainActivity extends FlutterActivity {
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//  //  GeneratedPluginRegistrant.registerWith(this);
+//
+//  }
+//}
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
